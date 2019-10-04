2d43d46bdcdc5bca854210f8184a42f0eebfd69c
Lundii <mondayemmanuel67@gmail.com>
2019-10-04 13:57:13 +0100

    Updates

---
+++ precache-manifest.954207d68e086c7b8bf37b529b46a2cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbeec0241bb189043644b0abe17d635",
+    "url": "/weather-app-frontend/index.html"
+  },
+  {
+    "revision": "deb842c20aab2c2cf7db",
+    "url": "/weather-app-frontend/static/js/2.1d0bb09e.chunk.js"
+  },
+  {
+    "revision": "5ae1bebdf9f3fc1e3a3b",
+    "url": "/weather-app-frontend/static/js/main.7132f684.chunk.js"
+  },
+  {
+    "revision": "37fbdcdb7eef9ec018f1",
+    "url": "/weather-app-frontend/static/js/runtime-main.f55e5553.js"
+  }
+]);
